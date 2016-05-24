81eba48a7bd1cb46d717c80be4303517695cce5a
Richard Bolt <richard.bolt@insidesales.com>
2016-05-24 17:01:50 -0600

    Package documentation in the main file for Godoc.

--- heartbeat.go
+++ heartbeat.go
@@ -1,3 +1,10 @@
+/*
+Package heartbeat is a small module to use with a Goa app providing a heartbeat to ping for health check purposes.
+
+By adding Heartbeat your app will respond with a 200 OK and a JSON object like so: `{"ENV":"production"}` on the url you specify or /health if no url is specified. Heartbeat returns a simple JSON object with one key, ENV, having the value of the ENV environment variable or "dev" if ENV has no value.
+
+Heartbeat is not a middleware, instead it adds itself to service.Mux as an http handler.
+*/
 package heartbeat
 
 import (
